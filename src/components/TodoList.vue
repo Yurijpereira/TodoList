0944eda846163c9de547d5b1aa0f9a56c2dd6cd2
yuri.jpereira <yuri.j.pereira2001@gmail.com>
2023-11-06 15:48:49 -0300

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <input type="text" placeholder="I need to..." v-model="newTodo" @keyup.enter="addTodo">
+        <button @click="addTodo">Add</button>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                {{ todo.title }}
+                <input type="checkbox" v-model="todo.completed">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+    data() {
+        return {
+            newTodo: null,
+            idForTodo: 1,
+            todos: [],
+        }
+    },
+    methods: {
+        addTodo() {
+            if (!this.newTodo) {
+                return;
+            } else {
+                this.todos.push({
+                    id: this.idForTodo,
+                    title: this.newTodo,
+                    completed: false,
+                });
+                this.idForTodo++;
+                this.newTodo = null
+            }
+        }
+    }
+}
+</script>
