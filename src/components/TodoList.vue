c334559314ab36586d256d320a0e191e43fb4bff
yuri.jpereira <yuri.j.pereira2001@gmail.com>
2023-11-08 13:38:16 -0300

    atualizando

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,21 +4,29 @@
         <button @click="addTodo">Add</button>
         <ul>
             <li v-for="todo in todos" :key="todo.id">
-                {{ todo.title }}
                 <input type="checkbox" v-model="todo.completed">
+                {{ todo.title }}
+                <button @click="update(todo)">{{!todo.buttonEdit ? "Edit" : "Save"}}</button>
+                <button @click="del(todo)">Delete</button>
+                <input v-batata v-if="todo.buttonEdit" v-model="updatedTodo" @keyup.enter="update(todo)" type="text">
             </li>
         </ul>
     </div>
 </template>
 
 <script>
+const batata = {
+    mounted: (el) => el.focus()
+}
+
 export default {
     name: 'TodoList',
     data() {
         return {
             newTodo: null,
-            idForTodo: 1,
+            idForTodo: 0,
             todos: [],
+            updatedTodo: null,
         }
     },
     methods: {
@@ -30,11 +38,24 @@ export default {
                     id: this.idForTodo,
                     title: this.newTodo,
                     completed: false,
+                    buttonEdit: false,
                 });
                 this.idForTodo++;
-                this.newTodo = null
+                this.newTodo = null;
             }
+        },
+        del(todo) {
+            let index = this.todos.indexOf(todo);
+            this.todos.splice(index, 1);
+        },
+        update(todo) {
+            todo.buttonEdit = !todo.buttonEdit;
+            todo.title = this.updatedTodo;
+            this.updatedTodo = null;
         }
+    },
+    directives: {
+        batata
     }
 }
 </script>
